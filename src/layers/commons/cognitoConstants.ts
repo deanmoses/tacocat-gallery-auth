56b008e6b3ccb3552139fac379111d12ba464d9a
Dean Moses <moses@tacocat.com>
2023-10-11 13:10:16 -0400

    Prepping to access Cognito

--- src/layers/commons/cognitoConstants.ts
+++ src/layers/commons/cognitoConstants.ts
@@ -1,5 +1,15 @@
-export const COGNITO_BASE_URI = "";
-export const COGNITO_LOGIN_CALLBACK_URI = "";
-export const COGNITO_LOGOUT_CALLBACK_URI = "";
-export const COGNITO_CLIENT_ID = "";
+export const COGNITO_BASE_URI = getEnv('CognitoBaseURI');
+export const COGNITO_LOGIN_CALLBACK_URI = getEnv('CognitoLoginCallbackURI');
+export const COGNITO_LOGOUT_CALLBACK_URI = getEnv('CognitoLogoutCallbackURI');
+export const COGNITO_POOL_ID = getEnv('CognitoUserPoolID');
+export const COGNITO_CLIENT_ID = getEnv('CognitoClientID');
 export const COGNITO_CLIENT_SECRET = "";
+export const WEB_APP_BASE_URI = getEnv('WebAppBaseURI');
+
+function getEnv(environmentVarName: string): string {
+    const environmentVarValue = process.env[environmentVarName];
+    if (!environmentVarValue) {
+        throw Error('No value for environment variable' + environmentVarName);
+    }
+    return environmentVarValue;
+}
