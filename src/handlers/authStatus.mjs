deanmoses/tacocat-gallery-auth

commit 5888698fa4e310d15618b2801b9a5a56b3cd435c

Unauthenticated returns 401 rather than 200
This fixes #2: authentication status API should return a 401 instead of 200 when not authenticated

src/handlers/authStatus.mjs
@@ -21,9 +21,6 @@ export const handler = async (event) => {
         throw new Error(`I only accept GET method, but instead I got: ${event.httpMethod}`);
     }
 
-	// The HTTP response we'll be building
-    let response = {};
-
 	// The user we'll be returning
     let user;
 
@@ -70,22 +67,27 @@ export const handler = async (event) => {
 		}
 	}
 
-    const body = {
-        user
-    }
-
-    response.statusCode = 200;
-	response.headers = {
-			"Content-Type": "application/json",
+	const response = {
+		isBase64Encoded: false,
+		headers: {
 			"Access-Control-Allow-Headers" : "X-Requested-With,Content-Type",
 			"Access-Control-Allow-Origin": `https://${GALLERY_APP_DOMAIN}`,
 			"Access-Control-Allow-Methods": "GET, OPTIONS",
 			"Access-Control-Allow-Credentials": "true",
 			'cache-control': 'no-cache, no-store, must-revalidate',
-		pragma: 'no-cache',
-		expires: 0,
-	};
-    response.body = JSON.stringify(body)
+			pragma: 'no-cache',
+			expires: 0,
+		}
+	}
+
+	if (!user) {
+		response.statusCode = 401;
+		response.body = JSON.stringify({errorMessage: 'Unauthorized'})
+	}
+	else {
+		response.statusCode = 200;
+		response.body = JSON.stringify({user})
+	}
 
     // All log statements are written to CloudWatch
     console.info(`response from: ${event.path} statusCode: ${response.statusCode} body: ${response.body}`);

commit 94820f4a51ee2fa2ea4426bd133eeedc8f782259

Bugfix: keep user logged in longer
 - Fixed bug where correct cookies were not being served that was introduced in 5888698fa4e310d15618b2801b9a5a56b3cd435c
 - Only serve cookies over HTTPS

This fixes #5

src/handlers/authStatus.mjs
@@ -21,6 +21,20 @@ export const handler = async (event) => {
         throw new Error(`I only accept GET method, but instead I got: ${event.httpMethod}`);
     }
 
+	// The response we'll be returning
+	const response = {
+		isBase64Encoded: false,
+		headers: {
+			"Access-Control-Allow-Headers" : "X-Requested-With,Content-Type",
+			"Access-Control-Allow-Origin": `https://${GALLERY_APP_DOMAIN}`,
+			"Access-Control-Allow-Methods": "GET, OPTIONS",
+			"Access-Control-Allow-Credentials": "true",
+			'cache-control': 'no-cache, no-store, must-revalidate',
+			pragma: 'no-cache',
+			expires: 0,
+		}
+	}
+
 	// The user we'll be returning
     let user;
 
@@ -44,16 +58,20 @@ export const handler = async (event) => {
 		// If the refresh token doesn't exist, user is not logged in
 		// If the refresh token DOES exist, try to get a new short-lived ID token
 		if (refreshToken) {
+			console.log('Found refresh token, trying to get new ID token from Cognito');
 			try {
 				// Try to update the tokens
 				const updatedTokens = await getTokensFromCognito({ refreshToken: refreshToken });
+
+				console.log('Got updated tokens from Cognito')
 
 				// Update the cookie for the id token
 				const idExpires = new Date();
 				idExpires.setSeconds(idExpires.getSeconds() + updatedTokens.expires_in);
                 response.multiValueHeaders = {
                     'Set-Cookie': [
-                        `id_token=${updatedTokens.id_token}; HttpOnly; Domain=tacocat.com; SameSite=Strict; Path=/; Expires=${idExpires}`
+                        `id_token=${updatedTokens.id_token}; HttpOnly; Secure; Domain=tacocat.com; SameSite=Strict; Path=/; Expires=${idExpires}`,
+						`was_authenticated=${"Authenticated at " + Date.now()}; Secure; Domain=tacocat.com; SameSite=Strict; Path=/; Expires=${idExpires}`
                     ]
                 }
 
@@ -62,21 +80,8 @@ export const handler = async (event) => {
 				user = { email: idToken.email };
 			} catch (error) {
 				// If the refresh token is invalid, treat user as not logged in
-				console.log('Authentication status error: ' + error);
+				console.error('Error getting refresh token from Cognito: ', error);
 			}
-		}
-	}
-
-	const response = {
-		isBase64Encoded: false,
-		headers: {
-			"Access-Control-Allow-Headers" : "X-Requested-With,Content-Type",
-			"Access-Control-Allow-Origin": `https://${GALLERY_APP_DOMAIN}`,
-			"Access-Control-Allow-Methods": "GET, OPTIONS",
-			"Access-Control-Allow-Credentials": "true",
-			'cache-control': 'no-cache, no-store, must-revalidate',
-			pragma: 'no-cache',
-			expires: 0,
 		}
 	}
 

src/handlers/loginCallback.mjs
@@ -50,8 +50,8 @@ export const handler = async (event) => {
         // Create the cookie headers
         const multiValueHeaders = {
             'Set-Cookie': [
-                `id_token=${tokens.id_token}; HttpOnly; Domain=tacocat.com; SameSite=Strict; Path=/; Expires=${idExpires}`,
-                `refresh_token=${tokens.refresh_token}; HttpOnly; Domain=tacocat.com; SameSite=Strict; Path=/; Expires=${refreshExpire}`,
+                `id_token=${tokens.id_token}; HttpOnly; Secure; Domain=tacocat.com; SameSite=Strict; Path=/; Expires=${idExpires}`,
+                `refresh_token=${tokens.refresh_token}; HttpOnly; Secure; Domain=tacocat.com; SameSite=Strict; Path=/; Expires=${refreshExpire}`,
                 // Set another cookie that simply tells the front end we are dealing with
                 // a user that MIGHT be authenticatable.  This is an optimization:
                 // Cookies with secure info (like a session ID) should be set as 
@@ -59,9 +59,9 @@ export const handler = async (event) => {
                 // client scripts.  This prevents them from being stolen by malicious scripts.
                 // Therefore, if I want the client to be able to short-circuit the logic
                 // and only call the authentication back end if there's an actual chance
-                // the user might be authenticated, I neeed to set another cookie with
+                // the user might be authenticated, I need to set another cookie with
                 // no sensitive information.
-                `was_authenticated=${"Authenticated at " + Date.now()}; Domain=tacocat.com; SameSite=Strict; Path=/; Expires=${idExpires}`
+                `was_authenticated=${"Authenticated at " + Date.now()}; Secure; Domain=tacocat.com; SameSite=Strict; Path=/; Expires=${idExpires}`
             ]
         }
 